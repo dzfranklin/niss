ed5b3c99f44f685c731d2cd07e3647b9b343d1dc
Daniel Franklin <daniel@danielzfranklin.org>
2021-10-10 19:39:29 -0600

    Show more logs

--- niss_fw/rootfs_overlay/etc/iex.exs
+++ niss_fw/rootfs_overlay/etc/iex.exs
@@ -3,5 +3,5 @@ NervesMOTD.print()
 # Add Toolshed helpers to the IEx session
 use Toolshed
 
-RingLogger.attach()
-RingLogger.tail(level: :warn)
+RingLogger.attach(level: :info)
+RingLogger.tail(level: :info)
