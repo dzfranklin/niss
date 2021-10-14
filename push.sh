0da55d22ff067387d3e83cee8f343a587e648577
Daniel Franklin <daniel@danielzfranklin.org>
2021-10-14 13:44:50 -0600

    Make push.sh position-independent

--- push.sh
+++ push.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+DIR=$(dirname "$0")
 
 export MIX_TARGET=rpi4
 export MIX_ENV=prod
 
-cd niss_ui || exit
-mix deps.get
-mix assets.deploy
-cd ../ || exit
+pushd "$DIR/niss_ui" && \
+	mix deps.get && \
+	mix assets.deploy && \
+	popd
 
-cd niss_fw || exit
-mix deps.get
-mix firmware
-mix upload home.danielzfranklin.org
+pushd "$DIR/niss_fw" && \
+	mix deps.get && \
+	mix firmware && \
+	mix upload home.danielzfranklin.org && \
+	popd
