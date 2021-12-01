b77326fba160059f0a03c6417147bbd7b34fcbd0
Daniel Franklin <daniel@danielzfranklin.org>
2021-12-01 01:30:51 -0700

    Add ability to get lights status

--- niss_local/lib/niss_local/lights.ex
+++ niss_local/lib/niss_local/lights.ex
@@ -8,27 +8,40 @@ defmodule NissLocal.Lights do
   """
   require Logger
 
-  def set_plug(name, status) do
-    config =
-      Application.fetch_env!(:niss_local, NissLocal.Lights)
-      |> Keyword.fetch!(:plugs)
-      |> Keyword.fetch!(name)
-
-    Logger.info("Setting #{inspect(name)} to #{inspect(status)}")
-
-    set_by_config(config, status)
+  def set!(name, status) do
+    fetch_config!(name)
+    |> run_tuya([
+      "set_status",
+      if(status, do: "true", else: "false")
+    ])
   end
 
-  defp set_by_config(config, status) do
+  def get!(name) do
+    {:ok, stdout} =
+      fetch_config!(name)
+      |> run_tuya(["get_status"])
+
+    case String.trim(stdout) do
+      "1" -> true
+      "0" -> false
+    end
+  end
+
+  def fetch_config!(name) do
+    Application.fetch_env!(:niss_local, NissLocal.Lights)
+    |> Keyword.fetch!(:plugs)
+    |> Keyword.fetch!(name)
+  end
+
+  defp run_tuya(config, args) do
     label = Keyword.fetch!(config, :label)
     id = Keyword.fetch!(config, :id)
     ip = Keyword.fetch!(config, :ip)
     key = Keyword.fetch!(config, :key)
-    status = if(status, do: "true", else: "false")
 
-    Logger.debug("Trying to set plug labelled #{inspect(label)} to #{inspect(status)}")
+    Logger.debug("Trying to manipulate plug labelled #{inspect(label)}: #{inspect(args)}")
 
-    {logs, status} =
+    {stdout, status} =
       System.cmd(
         tuya_bin(),
         [
@@ -37,19 +50,16 @@ defmodule NissLocal.Lights do
           "--ip",
           ip,
           "--key",
-          key,
-          "--status",
-          status
-        ],
-        stderr_to_stdout: true
+          key
+        ] ++ args
       )
 
     if status == 0 do
-      Logger.debug("Successfully set plug labelled #{inspect(label)} to #{inspect(status)}")
+      Logger.debug("Successfully manipulated plug labelled #{inspect(label)}")
+      {:ok, stdout}
     else
-      Logger.warn(
-        "Failed to set plug labelled #{inspect(label)} to #{inspect(status)}, got:\n#{logs}"
-      )
+      Logger.warn("Failed to manipulate plug labelled #{inspect(label)}, got stdout:\n#{stdout}")
+      :error
     end
   end
 
--- niss_local/priv/scripts/tuya.py
+++ niss_local/priv/scripts/tuya.py
@@ -4,6 +4,9 @@ import argparse
 import tinytuya
 
 
+# Parse arguments
+
+
 def parse_bool(val):
     val = str(val).upper()
     if val == "TRUE":
@@ -15,14 +18,40 @@ def parse_bool(val):
 
 
 parser = argparse.ArgumentParser(description="Control tuya outlet")
-parser.add_argument("--id")
-parser.add_argument("--ip")
-parser.add_argument("--key")
-parser.add_argument("--status", type=parse_bool)
+subparsers = parser.add_subparsers(dest="action")
+
+parser.add_argument("--id", required=True)
+parser.add_argument("--ip", required=True)
+parser.add_argument("--key", required=True)
+
+set_status_subparser = subparsers.add_parser("set_status")
+set_status_subparser.add_argument("status", type=parse_bool)
+
+get_status_subparser = subparsers.add_parser("get_status")
 
 args = parser.parse_args()
+
+
+# Set up plug
 
 plug = tinytuya.OutletDevice(args.id, args.ip, args.key)
 plug.set_version(3.3)
 
-plug.set_status(args.status)
+
+# Handle action
+
+
+def set_status(plug, args):
+    plug.set_status(args.status)
+
+
+def get_status(plug, _args):
+    status = plug.status()["dps"]["1"]
+    if status:
+        print("1")
+    else:
+        print("0")
+
+
+handlers = {"set_status": set_status, "get_status": get_status}
+handlers[args.action](plug, args)
