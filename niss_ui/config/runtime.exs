9f7afee7f1473c38397ebe64e0b07752b49bb81d
Daniel Franklin <daniel@danielzfranklin.org>
2021-12-03 16:55:54 -0700

    ui: Setup deploy

--- niss_ui/config/runtime.exs
+++ niss_ui/config/runtime.exs
@@ -8,15 +8,10 @@ import Config
 # The block below contains prod specific runtime configuration.
 if config_env() == :prod do
   database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+    System.get_env("UI_CONN_URL") || raise "environment variable UI_CONN_URL is missing."
 
   config :niss_ui, NissUi.Repo,
-    # ssl: true,
-    # socket_options: [:inet6],
+    socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
@@ -32,26 +27,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  # Remember endpoint also configured in config/prod.exs
+
   config :niss_ui, NissUiWeb.Endpoint,
+    server: true,
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
-
-  # ## Using releases
-  #
-  # If you are doing OTP releases, you need to instruct Phoenix
-  # to start each relevant endpoint:
-  #
-  #     config :niss_ui, NissUiWeb.Endpoint, server: true
-  #
-  # Then you can assemble a release by calling `mix release`.
-  # See `mix help release` for more information.
 
   # ## Configuring the mailer
   #
