fa0bc02ff232c4cf374eca3641e944aab1736c4e
Daniel Franklin <daniel@danielzfranklin.org>
2021-12-17 19:17:04 +0000

    Add a test case with real world data for sms

--- niss/test/niss/twilio_hook_test.exs
+++ niss/test/niss/twilio_hook_test.exs
@@ -74,4 +74,45 @@ defmodule Niss.TwilioHookTest do
 
     assert_receive {:new, ^new_text}
   end
+
+  test "saves and broadcasts with real world data" do
+    PubSub.subscribe("phone_text:+17208072089")
+
+    assert TwilioHook.message_in(%{
+             "AccountSid" => "AC5a9ae9ba61802c8d7b3f5facf9d5e6ed",
+             "ApiVersion" => "2010-04-01",
+             "Body" => "testing",
+             "From" => "+17207344134",
+             "FromCity" => "",
+             "FromCountry" => "US",
+             "FromState" => "CO",
+             "FromZip" => "",
+             "MessageSid" => "SM7bb6438617765a372a12c2e6392d6842",
+             "NumMedia" => "0",
+             "NumSegments" => "1",
+             "SmsMessageSid" => "SM7bb6438617765a372a12c2e6392d6842",
+             "SmsSid" => "SM7bb6438617765a372a12c2e6392d6842",
+             "SmsStatus" => "received",
+             "To" => "+17208072089",
+             "ToCity" => "",
+             "ToCountry" => "US",
+             "ToState" => "CO",
+             "ToZip" => ""
+           }) == nil
+
+    assert [
+             %Text{
+               message_sid: "SM7bb6438617765a372a12c2e6392d6842",
+               to_number: "+17208072089",
+               from_number: "+17207344134",
+               body: "testing",
+               from_city: nil,
+               from_state: "CO",
+               from_zip: nil,
+               from_country: "US"
+             } = new_text
+           ] = Phone.list_texts()
+
+    assert_receive {:new, ^new_text}
+  end
 end
