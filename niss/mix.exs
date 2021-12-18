89419ca2c8d123a354f31d4aed6cec4ef7c72f07
Daniel Franklin <daniel@danielzfranklin.org>
2021-12-18 11:17:35 +0000

    bugfix: move migrations directory to account for renamed repo

--- niss/mix.exs
+++ niss/mix.exs
@@ -65,7 +65,7 @@ defmodule Niss.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/local/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
