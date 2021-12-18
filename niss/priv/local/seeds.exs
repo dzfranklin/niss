d6130ba8a8da5f15147e9110f3ee95a33f3bf310
Daniel Franklin <daniel@danielzfranklin.org>
2021-12-18 11:17:35 +0000

    bugfix: move migrations directory to account for renamed repo

--- niss/priv/local/seeds.exs
+++ niss/priv/local/seeds.exs
@@ -1,6 +1,6 @@
 # Script for populating the database. You can run it as:
 #
-#     mix run priv/repo/seeds.exs
+#     mix run priv/local/seeds.exs
 #
 # Inside the script, you can read and write to any of your
 # repositories directly:
