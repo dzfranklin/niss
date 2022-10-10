22df27c7ac4e134c4cff2804e70824460a75de31
Daniel Franklin <daniel@danielzfranklin.org>
2022-10-10 11:18:42 -0600

    Revert "Store abandoned WIP"
    This reverts commit 3c721a4d26451c998a1414a705a3ad8dce5cb4d9.

--- core/groceries/src/store/migrations/cache/001_init.up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE
-    snapshots (as_of_seq_p INT, as_of_seq_q INT, data BLOB);
--- core/groceries/src/store/migrations/db/001_init.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-    events (
-        uuid BLOB,
-        seq_p INT,
-        seq_q INT,
-        event TEXT,
-        data_version INT,
-        data BLOB
-    );
